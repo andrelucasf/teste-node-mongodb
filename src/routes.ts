e9c1c1928e85772769f8e919035064c208fffd71
André Lucas <andre.lucas.lemos@hotmail.com>
2023-12-03 22:19:48 -0300

    feat: Add validators

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,38 @@
 // src/routes.ts
 import express from "express";
 import ProductController from "./modules/product/controller/ProductController";
+import {
+  createProductValidator,
+  productIdValidator,
+  updateProductValidator,
+} from "./modules/product/validators/ProductValidator";
 
 const router = express.Router();
 
-router.post("/products", ProductController.createProduct);
+router.post(
+  "/products",
+  createProductValidator,
+  ProductController.createProduct
+);
+
 router.get("/products", ProductController.listProducts);
-router.get("/products/:id", ProductController.getProductById);
-router.put("/products/:id", ProductController.updateProduct);
-router.delete("/products/:id", ProductController.deleteProduct);
+
+router.get(
+  "/products/:id",
+  productIdValidator,
+  ProductController.getProductById
+);
+
+router.put(
+  "/products/:id",
+  [productIdValidator, updateProductValidator],
+  ProductController.updateProduct
+);
+
+router.delete(
+  "/products/:id",
+  productIdValidator,
+  ProductController.deleteProduct
+);
 
 export default router;
